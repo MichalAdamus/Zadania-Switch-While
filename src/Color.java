8ab1a21722c02ef47934a64009d6bb972db7e0e9
Michał Adamus <mb.adamus@gmail.com>
2021-04-07 10:11:35 +0200

    #14 program do podawania kolorów

---
+++ src/Color.java
@@ -0,0 +1,15 @@
+public class Color {
+    public static void main(String[] args) {
+        int month = 'n';
+        switch (month){
+            case 'c':
+                System.out.println("Pomidor");break;
+            case 'z':
+                System.out.println("Trawa"); break;
+            case 'n':
+                System.out.println("morze");break;
+            default:
+                System.out.println("taki miesiąc nie istnieje");
+        }
+    }
+}
