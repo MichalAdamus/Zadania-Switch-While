7ab5b580a3e859f87f40314ac06d96bac575ce5b
Michał Adamus <mb.adamus@gmail.com>
2021-04-08 09:05:08 +0200

    #16 dodanie rozwiązania za pomocą switch

--- src/Salary.java
+++ src/Salary.java
@@ -2,16 +2,18 @@ public class Salary {
     public static void main(String[] args) {
         int salary = 2000;
         int newSalary = 0;
-        if (salary == 100) {
-            newSalary = salary + 1000;
+        switch (salary) {
+            case 100:
+                newSalary = salary + 1000;
+                break;
+            case 2000:
+                newSalary = salary + 200;
+                break;
+            case 2500:
+                newSalary = salary + 25;
+                break;
         }
-        if (salary == 2000) {
-            newSalary = salary + 200;
-        }
-        if (salary == 2500) {
-            newSalary = salary + 25;
-        }
-        double i = ((double)newSalary/ salary)*100-100;
+        double i = ((double) newSalary / salary) * 100 - 100;
         System.out.println("Salary: " + salary);
         System.out.println("New Salary: " + newSalary);
         System.out.println("Brawo ! Twoja wypłata jest wyższa o " + i + "%");
