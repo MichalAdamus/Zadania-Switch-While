c23ec62d965143d5e3abdea7ab78ec5ec072b1cd
Michał Adamus <mb.adamus@gmail.com>
2021-04-08 08:57:22 +0200

    #16 poprawione zadanie z salary

--- src/Salary.java
+++ src/Salary.java
@@ -1,11 +1,19 @@
 public class Salary {
     public static void main(String[] args) {
         int salary = 2000;
-        int newSalary = salary > 2000 ?salary : salary +200;
-
+        int newSalary = 0;
+        if (salary == 100) {
+            newSalary = salary + 1000;
+        }
+        if (salary == 2000) {
+            newSalary = salary + 200;
+        }
+        if (salary == 2500) {
+            newSalary = salary + 25;
+        }
+        double i = ((double)newSalary/ salary)*100-100;
         System.out.println("Salary: " + salary);
         System.out.println("New Salary: " + newSalary);
-        var i = (newSalary / salary)*10;
-        System.out.println("Brawo ! Twoja wypłata jest wyższa o "+ i +"%");
+        System.out.println("Brawo ! Twoja wypłata jest wyższa o " + i + "%");
     }
 }
